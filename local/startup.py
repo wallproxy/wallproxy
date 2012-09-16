875621501a32069c0ef0702f659a006b5ba19c0c
wallproxy <www.ehust@gmail.com>
2012-09-16 16:09:03 +0800

    CA.crt can be accessed from web, startup.py can be renamed to proxy.py

--- local/startup.py
+++ local/startup.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
-import sys, os.path as ospath
+import sys, os, os.path as ospath
+#os.environ['DISABLE_GEVENT'] = '1'
 dir = ospath.dirname(sys.argv[0])
-sys.path.append(ospath.abspath(ospath.join(dir, 'src.zip')))
-del sys, ospath, dir
+sys.path.insert(0, ospath.abspath(ospath.join(dir, 'src.zip')))
+del sys, os, ospath, dir
 from proxy import main
 main()
