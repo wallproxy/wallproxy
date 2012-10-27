361553da90018fa046e3f74d7f1cab8316bf47c0
wallproxy <www.ehust@gmail.com>
2012-10-27 21:02:15 +0800

    v2.0.13; change make_config.py; fix https path bug

--- server/uploader.py
+++ server/uploader.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
-import sys, os.path as ospath
+import sys, os, os.path as ospath
 dir = ospath.dirname(sys.argv[0])
+os.chdir(dir)
 sys.argv[1:] = [ospath.join(dir, 'uploader')]
 sys.path.append(ospath.abspath(ospath.join(dir, '../local/src.zip')))
 del sys, ospath, dir
