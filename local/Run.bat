8b7bfd7ee6426608520fd4fcf6ec6dfda4c5ef85
wallproxy <www.ehust@gmail.com>
2012-11-15 20:19:25 +0800

    v2.1.5; fix Ctrl+C cannot exit if no gevent; pac make_config; close socket before POST; fix https proxy bug

--- local/Run.bat
+++ local/Run.bat
@@ -3,4 +3,4 @@ rem --hide运行后隐藏窗口；--single重复运行时激活旧窗口，而非再开一个窗口，--exit
 rem 可组合，例如--hide与--single合用，首次运行自动隐藏窗口，再次运行显示旧窗口
 rem 建议将python.exe发送到桌面快捷方式并加上--hide --single参数
 rem 如需开机自动运行，将快捷方式放到开始菜单的启动文件夹
-start python.exe --hide --single
+start "WallProxy" "%~dp0python.exe" --hide --single
