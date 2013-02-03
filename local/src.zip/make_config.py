18d45d0a12bf594ef8a174e0af3c71a3e0ff46a2
wallproxy <www.ehust@gmail.com>
2013-02-03 15:12:20 +0800

    merge some changes form dev

--- local/src.zip/make_config.py
+++ local/src.zip/make_config.py
@@ -236,7 +236,7 @@ class Common(object):
                 v = v.replace('|', ' ').strip()
                 if k and v: self.HOSTS[k] = v
 
-        self.THIRD_APPS = None
+        self.THIRD_APPS = []
         if self.getboolean('third', 'enable', CONFIG.has_section('third')):
             self.remove_option('third', 'enable', '')
             self.THIRD_APPS = [(k,v if v[0] in ('"',"'") else repr(v)) for k,v in self.items('third', ()) if v]
