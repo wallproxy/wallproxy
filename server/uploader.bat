eb345216b9dceec38baacf26a043bb297987ae53
wallproxy <www.ehust@gmail.com>
2014-04-23 20:36:20 +0800

    update uploader

--- server/uploader.bat
+++ server/uploader.bat
@@ -6,8 +6,8 @@ set uploaddir=python
     echo ===============================================================
     echo  WallProxy服务端部署程序, 开始上传%uploaddir%服务端
     echo ===============================================================
-    echo.
-    echo 请输入您的appid, 多个appid请用^|号隔开
+rem     echo.
+rem     echo 请输入您的appid, 多个appid请用^|号隔开
 ) && (
     @cd /d "%~dp0" 
 ) && (
