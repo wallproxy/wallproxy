a29a2649d429f1e734701255b24f5f9d43513faf
wallproxy <www.ehust@gmail.com>
2014-05-25 17:04:57 +0800

    add redirects.ini

--- local/startup.sh
+++ local/startup.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-python2 "${0%/*}/startup.py"
-read -n1
+if [ -L "$0" ]; then
+	DIR=`readlink "$0"`
+else
+	DIR=$0
+fi
+DIR=`dirname "$DIR"`
+python2 "$DIR/startup.py" "$DIR/pac" proxy.pac >/dev/null 2>&1 &
+SID=`ps -p $$ -o sid --no-headers`
+if [ "$SID" = "$$" ]; then
+	python2 "$DIR/startup.py" &
+	cat <&0 >/dev/null
+else
+	python2 "$DIR/startup.py"
+fi
